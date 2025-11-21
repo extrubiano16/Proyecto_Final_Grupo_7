ef3b9717927e194888e2bd92adeeab444d446db3
Samuel-VR <samuel.verano@est.uexternado.edu.co>
2025-11-21 05:38:36 +0000

    svr

--- pandas/funciones.py
+++ pandas/funciones.py
@@ -7,3 +7,14 @@ def promedios(nombre_archivo, nombre_columna):
         return "No existe la columna"
     else:
         return df[nombre_columna].mean()
+def desviacion_v2(nombre_archivo, columna):
+    try:
+        df = pd.read_csv(nombre_archivo)
+        if columna not in df.columns:
+            return "Error al procesar la columna"
+        numeric_values = pd.to_numeric(df[columna], errors='coerce') 
+        numeric_values = numeric_values.dropna()  
+        return round(numeric_values.std(), 2)
+    except Exception as e:
+        return f"Error al procesar la columna: {e}"
+  
